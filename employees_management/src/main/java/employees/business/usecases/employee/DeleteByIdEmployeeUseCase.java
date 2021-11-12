fcf4923ced713add2e22e5ab009a76a1c3875fea
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-11-12 15:10:23 -0300

    refactor: add lombok to create all args contructor

--- employees_management/src/main/java/employees/business/usecases/employee/DeleteByIdEmployeeUseCase.java
+++ employees_management/src/main/java/employees/business/usecases/employee/DeleteByIdEmployeeUseCase.java
@@ -3,13 +3,12 @@ package employees.business.usecases.employee;
 import employees.business.module.errors.employee.EmployeeErrors;
 import employees.business.repositories.IEmployeeRepository;
 import employees.business.usecases.IUseCaseContract;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class DeleteByIdEmployeeUseCase implements IUseCaseContract<String, Void> {
 
   private IEmployeeRepository repository;
-  public DeleteByIdEmployeeUseCase(IEmployeeRepository repository) {
-    this.repository = repository;
-  }
 
   @Override
   public Void exec(String id) {
