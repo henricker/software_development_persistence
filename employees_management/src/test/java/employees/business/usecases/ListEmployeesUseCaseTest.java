393722bb6a16b2bb51e126b0dd83eadfde6a3f49
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-11-12 12:03:50 -0300

    test: should ListEmployeesUseCase return list of employees on success

--- employees_management/src/test/java/employees/business/usecases/ListEmployeesUseCaseTest.java
+++ employees_management/src/test/java/employees/business/usecases/ListEmployeesUseCaseTest.java
@@ -1,10 +1,15 @@
 package employees.business.usecases;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import employees.business.repositories.IEmployeeRepository;
 import employees.business.usecases.employee.ListEmployeesUseCase;
+import employees.domain.entities.Employee;
 import employees.mocks.dto.employee.FakeEmployeeDTOFactory;
 import employees.mocks.repositories.FakeEmployeeRepository;
 
@@ -18,7 +23,15 @@ public class ListEmployeesUseCaseTest {
     useCase.exec(FakeEmployeeDTOFactory.listEmployeeInput());
 
     Mockito.verify(repo).findAll(0, 10);
-    
   }
+
+  @Test
+  public void shouldReturnAListWithEmployees() {
+    ListEmployeesUseCase useCase = new ListEmployeesUseCase(new FakeEmployeeRepository());
+    List<Employee> employees = useCase.exec(FakeEmployeeDTOFactory.listEmployeeInput());
+    
+    assertEquals(employees, new FakeEmployeeRepository().findAll(0, 10));
+  }
+
 
 }
