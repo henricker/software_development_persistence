e5e6c5fe908c6e00f070cf8332da4863f6a53a1d
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-11-12 08:32:42 -0300

    init java project using maven

---
+++ Exercise_List_04/src/main/java/employees/App.java
@@ -0,0 +1,15 @@
+package employees;
+
+import employees.domain.entities.Employee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Employee e = new Employee();
+    }
+}
