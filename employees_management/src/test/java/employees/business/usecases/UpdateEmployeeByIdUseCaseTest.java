2ad6caf580ba11e877c2ff179184358346e860bb
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-11-12 15:40:27 -0300

    test: should UpdateEmployeeByIdUseCase return employee error if employee not found

--- employees_management/src/test/java/employees/business/usecases/UpdateEmployeeByIdUseCaseTest.java
+++ employees_management/src/test/java/employees/business/usecases/UpdateEmployeeByIdUseCaseTest.java
@@ -1,9 +1,12 @@
 package employees.business.usecases;
 
+import static org.mockito.Mockito.when;
+
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import employees.business.dto.employee.UpdateEmployeeInputDTO;
+import employees.business.module.errors.employee.EmployeeErrors;
 import employees.business.repositories.IEmployeeRepository;
 import employees.business.usecases.employee.UpdateEmployeeByIdUseCase;
 import employees.mocks.repositories.FakeEmployeeRepository;
@@ -26,6 +29,23 @@ public class UpdateEmployeeByIdUseCaseTest {
     );
 
     Mockito.verify(repo).findBy("id", "valid_id");
+  }
+
+  @Test(expected = EmployeeErrors.class)
+  public void shouldReturnEmployeeErrorIfEmployeeNotFound() {
+    IEmployeeRepository repo = Mockito.spy(new FakeEmployeeRepository());
+    when(repo.findBy("id", "valid_id")).thenReturn(null);
+    UpdateEmployeeByIdUseCase useCase = new UpdateEmployeeByIdUseCase(repo);
+
+    useCase.exec(
+      new UpdateEmployeeInputDTO(
+        "valid_id", 
+        "valid_registration", 
+        "valid_name", 
+        "valid_email", 
+        "valid_phone"
+      )
+    );
   }
 
 }
