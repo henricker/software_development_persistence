50a5af1e397a018c3ac1238ee92890c3e60dd118
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-11-13 15:53:38 -0300

    feat: should CreateEmployeeUseCase calls UniqueIndentifierService

--- employees_management/src/main/java/br/quixada/dspersist/employees/business/usecases/employee/CreateEmployeeUseCase.java
+++ employees_management/src/main/java/br/quixada/dspersist/employees/business/usecases/employee/CreateEmployeeUseCase.java
@@ -3,12 +3,14 @@ package br.quixada.dspersist.employees.business.usecases.employee;
 import br.quixada.dspersist.employees.business.dto.employee.CreateEmployeeInputDTO;
 import br.quixada.dspersist.employees.business.module.errors.employee.EmployeeErrors;
 import br.quixada.dspersist.employees.business.repositories.IEmployeeRepository;
+import br.quixada.dspersist.employees.business.services.uniqueIndentifier.IUniqueIndentifier;
 import br.quixada.dspersist.employees.business.usecases.IUseCaseContract;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class CreateEmployeeUseCase implements IUseCaseContract<CreateEmployeeInputDTO, Void> {
   private IEmployeeRepository employeeRepository;
+  private IUniqueIndentifier uniqueIndentifier;
 
   @Override
   public Void exec(CreateEmployeeInputDTO data) {
@@ -23,7 +25,7 @@ public class CreateEmployeeUseCase implements IUseCaseContract<CreateEmployeeInp
     if(registrationExists)
       throw EmployeeErrors.registrationAlreadyExistsError();
 
-
+    data.setId(uniqueIndentifier.create());
     this.employeeRepository.create(data.map());
       
     return null;
