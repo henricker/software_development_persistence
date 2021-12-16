d9450cbb750f18e2467cf26d2e7c036d2349b32e
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-12-16 10:07:36 -0300

    chore: init project

---
+++ Assignment_02/src/test/java/br/com/ufc/quixada/dspersist/schoolmanagement/SchoolManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ufc.quixada.dspersist.schoolmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
