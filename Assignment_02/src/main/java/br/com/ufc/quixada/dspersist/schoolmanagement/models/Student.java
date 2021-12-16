48c4a7cfa06f01b1a9ad9fc60d0764484987d7f4
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-12-16 11:29:14 -0300

    refactor: remove unused imports

--- Assignment_02/src/main/java/br/com/ufc/quixada/dspersist/schoolmanagement/models/Student.java
+++ Assignment_02/src/main/java/br/com/ufc/quixada/dspersist/schoolmanagement/models/Student.java
@@ -3,18 +3,14 @@ package br.com.ufc.quixada.dspersist.schoolmanagement.models;
 import java.time.LocalDate;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
