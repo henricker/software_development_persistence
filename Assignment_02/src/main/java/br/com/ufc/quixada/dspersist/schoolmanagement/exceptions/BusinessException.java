59f59e0999e175b6cb994a7d81eda08b90eb85d3
Henrique Vieira <henriquevieira@alu.ufc.br>
2022-01-10 09:41:15 -0300

    feat: add method to show message error

--- Assignment_02/src/main/java/br/com/ufc/quixada/dspersist/schoolmanagement/exceptions/BusinessException.java
+++ Assignment_02/src/main/java/br/com/ufc/quixada/dspersist/schoolmanagement/exceptions/BusinessException.java
@@ -11,4 +11,12 @@ public class BusinessException extends RuntimeException {
     this.code = code;
     this.shortMessage = shortMessage;
   }
+
+  public String showMessage() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("---- " + this.shortMessage + "----\n");
+    sb.append("code: " + this.code + "\n");
+    sb.append("message: " + this.getMessage());
+    return sb.toString();
+  }
 }
